b6a4ffaaa2ffe547f1386ee904405e6195731638
TorroSoft <torrozapata@gmail.com>
2021-06-29 12:51:10 -0700

    Agregando el plugin gulp-webp y generando imagenes webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { src, dest, series, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
 
 //Compilando SASS
 function css() {
@@ -14,11 +16,18 @@ function imageMin() {
     return src("./src/img/**/*")
         .pipe(imagemin())
         .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Imagen Minificada" }));
+}
+
+function imageWebp() {
+    return src("./src/img/**/*")
+        .pipe(webp())
+        .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Imagen webp lista" }));
 }
 
 function observa() {
     watch("./src/scss/**/*.scss", css);
 }
 
-exports.default = series(css, observa);
-//exports.imageMin = imageMin;
+exports.default = series(css, imageMin, imageWebp, observa);
