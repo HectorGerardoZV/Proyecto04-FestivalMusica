9acb8fb857f5d740a6ea23e9689a33bca1915e25
TorroSoft <torrozapata@gmail.com>
2021-06-29 12:30:54 -0700

    Minificando imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require("gulp-imagemin");
 
 //Compilando SASS
 function css() {
@@ -9,8 +10,15 @@ function css() {
 
 }
 
+function imageMin() {
+    return src("./src/img/**/*")
+        .pipe(imagemin())
+        .pipe(dest("./build/img"))
+}
+
 function observa() {
     watch("./src/scss/**/*.scss", css);
 }
 
 exports.default = series(css, observa);
+//exports.imageMin = imageMin;
