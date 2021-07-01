a5dde330399289dc156ab7c6e6738aa92239e9b0
TorroSoft <torrozapata@gmail.com>
2021-07-01 11:58:05 -0700

    Finalizacion del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,32 @@ const notify = require("gulp-notify");
 const webp = require("gulp-webp");
 const concat = require("gulp-concat");
 
+//Utilidades css
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const sourcemap = require("gulp-sourcemaps");
+const rename = require("gulp-rename");
+
+//Utilidades js
+const terser = require("gulp-terser-js");
 //Compilando SASS
 function css() {
     return src("./src/scss/app.scss")
+        .pipe(sourcemap.init())
         .pipe(sass())
+        .pipe(postcss(cssnano()))
+        .pipe(sourcemap.write("."))
         .pipe(dest("./build/css"));
 
 }
 
 function js() {
     return src("./src/js/**/*.js")
+        .pipe(sourcemap.init())
         .pipe(concat("bundle.js"))
+        .pipe(terser())
+        .pipe(sourcemap.write("."))
+        .pipe(rename({ suffix: ".min" }))
         .pipe(dest("./build/js"))
 }
 
