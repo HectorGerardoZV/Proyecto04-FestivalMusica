357744c15bfe955c81ea77650d257ae648c50369
TorroSoft <torrozapata@gmail.com>
2021-06-30 21:26:39 -0700

    Finalizacion de la funcionalidad de la seccion de galeria

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -10,8 +10,35 @@ function crearGaleria() {
         const imagen = document.createElement("IMG");
         imagen.src = `../build/img/thumb/${i}.webp`;
         imagen.classList.add("galeria__img");
+        imagen.dataset.imagenId = i;
+        imagen.onclick = abreImagenGrande;
         const lista = document.createElement("LI");
         lista.appendChild(imagen);
         galeria.appendChild(lista);
     }
+
+
+}
+
+
+function abreImagenGrande(e) {
+    const id = parseInt(e.target.dataset.imagenId);
+    console.log(id);
+    const imagen = document.createElement("IMG");
+    imagen.src = `../build/img/grande/${id}.webp`;
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay")
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("hidden-body")
+    overlay.onclick = function() {
+        body.removeChild(overlay);
+        body.classList.remove("hidden-body");
+        body.classList.add("scroll-body");
+
+    }
+
+
+
 }
